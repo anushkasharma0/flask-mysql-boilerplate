6d42b478581247a5c4e0f1559344c7915df7eae4
codergirl7588 <anushkak.sharma21@gmail.com>
2023-04-18 11:53:26 -0400

    restaurant post route

--- flask-app/src/restaurant/restaurant.py
+++ flask-app/src/restaurant/restaurant.py
@@ -142,5 +142,31 @@ def get_menu_rating(restaurantId):
     return the_response
 
 # Post information about new restaurant
+@restaurant.route('/restaurant', methods=['POST'])
+def add_new_restaurant():
+     
+     # collecting data from the request object
+     the_data = request.json
+     current_app.logger.info(the_data)
+
+
+     # extracting the variable
+     name = the_data['name']
+     rating = the_data['rating']
+     price = the_data ['price_range']
+     
+     # constructing the query
+     query = 'insert into products (name, rating, price_range) values ("'
+     query += name + '", "'
+     query += str(rating) + ', '
+     query += price + '")"'
+     current_app.logger.info(query)
+     
+     # executing and committing the insert statement
+     cursor = db.get_db().cursor()
+     cursor.execute(query)
+     db.get_db().commit()
+
+     return 'Success!'
 # Put
 # Delete
