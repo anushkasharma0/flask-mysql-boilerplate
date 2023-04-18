967a15b74c9b3c18f8c51e52d26b4ca469d4fb9e
codergirl7588 <anushkak.sharma21@gmail.com>
2023-04-18 14:14:54 -0400

    comment

--- flask-app/src/restaurant/restaurant.py
+++ flask-app/src/restaurant/restaurant.py
@@ -141,7 +141,7 @@ def get_menu_rating(restaurantId):
     the_response.mimetype = 'application/json'
     return the_response
 
-# Post information about new restaurant - fix this
+# Post information about new restaurant
 @restaurant.route('/restaurant', methods=['POST'])
 def add_new_restaurant():
      
