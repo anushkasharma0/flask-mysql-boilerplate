099130d7fbd7469505b8c0566eb728ff8d541545
codergirl7588 <anushkak.sharma21@gmail.com>
2023-04-17 20:20:51 -0400

    2 restaurant routes

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -33,10 +33,12 @@ def create_app():
     from src.views import views
     from src.customers.customers import customers
     from src.products.products  import products
+    from src.restaurant.restaurant import restaurant
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
     app.register_blueprint(customers,   url_prefix='/c')
     app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(restaurant,  url_prefix='/r')
 
     return app
---
+++ flask-app/src/restaurant/restaurant.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+
+restaurant = Blueprint('restaurant', __name__)
+
+# Get all restaurants from the DB
+@restaurant.route('/restaurant', methods=['GET'])
+def get_restaurants():
+    cursor = db.get_db().cursor()
+    cursor.execute('select name, rating,\
+        price_range from restaurant')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get restaurant detail for restaurant with particular restaurantId
+@restaurant.route('/restaurant/<restaurantId>', methods=['GET'])
+def get_restaurant_data(restaurantId):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from restaurant where id = {0}'.format(restaurantId))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
